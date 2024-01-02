2c2c66eb11f71c292d62a17b31bdab2df05c109b
unso99 <unso99@naver.com>
2024-01-02 23:16:51 +0900

    Design: input 화면 구성

---
+++ app/src/main/java/com/myboard/InputActivity.kt
@@ -0,0 +1,15 @@
+package com.myboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.myboard.databinding.ActivityInputBinding
+
+class InputActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityInputBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInputBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
