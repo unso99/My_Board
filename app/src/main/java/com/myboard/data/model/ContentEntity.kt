8482e30737831c2a7fd04ebd0d291a7edd609c96
kimgdongju <unso99@gamil.com>
2024-01-03 12:24:42 +0900

    Add: ContentEntity 추가

---
+++ app/src/main/java/com/myboard/data/model/ContentEntity.kt
@@ -0,0 +1,25 @@
+package com.myboard.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+import java.util.Date
+
+
+//api 통신을 통해 가져오는 data
+@Entity(tableName = "Content")
+data class ContentEntity(
+    @PrimaryKey(false)
+    val id : Int,
+    @ColumnInfo
+    var title : String,
+    @ColumnInfo
+    var content : String,
+    @ColumnInfo
+    val createdDate : Date,
+    @ColumnInfo
+    val likeCount : Int,
+    @ColumnInfo
+    val viewCount : Int
+) : Serializable
