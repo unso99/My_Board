15c17bc9880b48d8760bd412756402f09713f4b8
kimgdongju <unso99@gamil.com>
2024-01-04 17:49:12 +0900

    Feat: retrofit service 추가

---
+++ app/src/main/java/com/myboard/presentation/di/ServiceModule.kt
@@ -0,0 +1,18 @@
+package com.myboard.presentation.di
+
+import com.myboard.data.source.remote.api.ContentService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+
+    @Provides
+    @Singleton
+    fun providesContentService(retrofit: Retrofit) : ContentService = retrofit.create(ContentService::class.java)
+}
