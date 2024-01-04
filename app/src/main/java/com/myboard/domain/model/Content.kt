80b53bfe88fc1be250fa3bfcbd826b1c17fc1795
kimgdongju <unso99@gamil.com>
2024-01-04 18:01:02 +0900

    Feat: domain Content model 추가

---
+++ app/src/main/java/com/myboard/domain/model/Content.kt
@@ -0,0 +1,14 @@
+package com.myboard.domain.model
+
+import java.io.Serializable
+import java.util.Date
+
+//클린 아키텍처를 위해 presenter가 domain에서 사용할 model
+data class Content(
+    val id: Int? = null,
+    val title: String,
+    val content: String,
+    val createdDate: Date = Date(),
+    val likeCount: Int = 0,
+    val viewCount: Int = 0,
+) : Serializable
