e8ccd5efd618c8f9a7afa8fdd0700a9edad3b441
kimgdongju <unso99@gamil.com>
2024-01-04 17:37:30 +0900

    Feat: retrofit 모듈 생성

---
+++ app/src/main/java/com/myboard/presentation/di/RetrofitModule.kt
@@ -0,0 +1,53 @@
+import com.google.gson.GsonBuilder
+import com.myboard.util.DateUtil
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    @Singleton
+    fun providesConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create(
+            GsonBuilder()
+                .setDateFormat(DateUtil.serverDateFormat.toPattern())
+                .create()
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient.Builder {
+        return OkHttpClient.Builder().apply {
+            readTimeout(5, TimeUnit.SECONDS)
+            connectTimeout(5, TimeUnit.SECONDS)
+            writeTimeout(5, TimeUnit.SECONDS)
+            addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(
+        client: OkHttpClient.Builder,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://localhost:3000")
+            .addConverterFactory(gsonConverterFactory)
+            .client(client.build())
+            .build()
+    }
+}
--- app/src/main/java/com/myboard/util/DateUtil.kt
+++ app/src/main/java/com/myboard/util/DateUtil.kt
@@ -7,7 +7,7 @@ object DateUtil {
 
     val yyyy_MM_dd = SimpleDateFormat("yyyy-MM-dd")
     val dbDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
-    val serverDateFormat = SimpleDateFormat("yyyy-MM=dd'T'HH:mm:ss.SSS")
+    val serverDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
 
     fun convertPrintDateString(date: Date? = Date()) {
         yyyy_MM_dd.format(date)
