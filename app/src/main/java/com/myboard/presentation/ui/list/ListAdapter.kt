d4fe257e51c5765e1a4d3827d95298ce8be58395
kimgdongju <unso99@gamil.com>
2024-01-05 12:42:56 +0900

    Feat: listAdapter 추가

---
+++ app/src/main/java/com/myboard/presentation/ui/list/ListAdapter.kt
@@ -0,0 +1,37 @@
+package com.myboard.presentation.ui.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.myboard.databinding.ItemContentBinding
+import com.myboard.domain.model.Content
+import com.myboard.presentation.ui.list.viewholder.ContentViewHolder
+
+class ListAdapter() : ListAdapter<Content, ContentViewHolder>(diffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentViewHolder {
+        return ContentViewHolder(
+            ItemContentBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ContentViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffCallback = object : DiffUtil.ItemCallback<Content>() {
+            override fun areItemsTheSame(oldItem: Content, newItem: Content): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Content, newItem: Content): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
