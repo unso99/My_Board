34edec580d6a226286625b1165b37367166b93a6
kimgdongju <unso99@gamil.com>
2024-01-05 12:49:41 +0900

    Feat: Handler 클래스 추가

--- app/src/main/java/com/myboard/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/myboard/presentation/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package com.myboard.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.myboard.databinding.ActivityMainBinding
+import com.myboard.domain.model.Content
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -14,6 +16,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickAdd() {
+
+    }
+
+    inner class Handler {
+        fun onClick(item: Content) {
+
+        }
+
+        fun onLongClick(item: Content): Boolean {
+            AlertDialog.Builder(this@MainActivity)
+                .setTitle("정말 삭제하시겠습니까?")
+                .setPositiveButton("네") { _, _ ->
+
+                }
+                .setNegativeButton("아니오") { _, _ ->
+
+                }
+                .show()
+            return false
+        }
 
     }
 }
