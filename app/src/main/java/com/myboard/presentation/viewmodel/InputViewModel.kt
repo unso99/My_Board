49055975ce60f8eacbd1792d906210fae797d7d1
unso99 <unso99@naver.com>
2024-01-07 23:33:06 +0900

    Feat: 데이터 추가 usecase 기능 추가

--- app/src/main/java/com/myboard/presentation/viewmodel/InputViewModel.kt
+++ app/src/main/java/com/myboard/presentation/viewmodel/InputViewModel.kt
@@ -3,11 +3,18 @@ package com.myboard.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.myboard.domain.model.Content
+import com.myboard.domain.usecase.ContentUseCase
 import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @ViewModelScoped
-class InputViewModel : ViewModel() {
+class InputViewModel @Inject constructor(
+    private val contentUseCase: ContentUseCase
+) : ViewModel() {
     private val _doneEvent = MutableLiveData<Pair<Boolean, String>>()
     val doneEvent: LiveData<Pair<Boolean, String>> = _doneEvent
 
@@ -24,7 +31,7 @@ class InputViewModel : ViewModel() {
         content.value = item.content
     }
 
-    fun insertData(){
+    fun insertData(item : Content){
         val nicknameValue = nickname.value
         val titleValue = title.value
         val contentValue= content.value
@@ -34,6 +41,17 @@ class InputViewModel : ViewModel() {
              contentValue.isNullOrBlank()){
             _doneEvent.value = Pair(false,"모든 항목을 입력하셔야 합니다.")
             return
+        }
+
+        //usecase를 통해 데이터가 추가됨
+        viewModelScope.launch(Dispatchers.IO){
+            contentUseCase.save(item?.copy(
+                nickName = nicknameValue,
+                title = titleValue,
+                content = contentValue
+            ) ?: Content(nickName = nicknameValue, title = titleValue, content = contentValue))
+        }.also {
+            _doneEvent.postValue(Pair(true,if(it as Boolean) "완료!" else "저장할 수 없습니다."))
         }
     }
 }
