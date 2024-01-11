0128d03294d56ca6f7f38e4a448a2d814ba8a981
kimgdongju <unso99@gamil.com>
2024-01-11 12:49:24 +0900

    Feat: 갤러리에서 이미지 불러오기 기능 추가

--- app/src/main/java/com/myboard/presentation/ui/InputActivity.kt
+++ app/src/main/java/com/myboard/presentation/ui/InputActivity.kt
@@ -2,8 +2,12 @@ package com.myboard.presentation.ui
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.myboard.databinding.ActivityInputBinding
@@ -14,8 +18,15 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class InputActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityInputBinding
-    private val viewModel : InputViewModel by viewModels()
+    private lateinit var binding: ActivityInputBinding
+    private val viewModel: InputViewModel by viewModels()
+    private val imageLoadLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            uri?.let {
+                Log.e("imageUrl", uri.toString())
+                setImage(uri)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +34,13 @@ class InputActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+        binding.view = this
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         (intent.getSerializableExtra(ITEM) as? Content)?.let {
             viewModel.initItem(it)
         }
         observeViewModel()
-        PermissionUtil.checkPermission(this)
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -38,20 +49,28 @@ class InputActivity : AppCompatActivity() {
     }
 
     //done이벤트 감시
-    private fun observeViewModel(){
-        viewModel.doneEvent.observe(this){
-            Toast.makeText(this,it.second,Toast.LENGTH_SHORT).show()
-            if(it.first) finish()
+    private fun observeViewModel() {
+        viewModel.doneEvent.observe(this) {
+            Toast.makeText(this, it.second, Toast.LENGTH_SHORT).show()
+            if (it.first) finish()
         }
     }
 
-    companion object{
-        private  const val ITEM = "item"
+    private fun setImage(uri: Uri) {
+        binding.imageView.setImageURI(uri)
+    }
+
+    fun onImageClick() {
+        PermissionUtil.checkPermission(this, imageLoadLauncher)
+    }
+
+    companion object {
+        private const val ITEM = "item"
 
         //item을 받아오는 함수
-        fun start(context: Context, item : Content? = null){
-            Intent(context,InputActivity::class.java).apply {
-                putExtra(ITEM,item)
+        fun start(context: Context, item: Content? = null) {
+            Intent(context, InputActivity::class.java).apply {
+                putExtra(ITEM, item)
             }.run {
                 context.startActivity(this)
             }
