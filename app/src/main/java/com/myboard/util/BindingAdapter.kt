8ca0c357592f9826055e5139d634c984ba72d9db
kimgdongju <unso99@gamil.com>
2024-01-12 11:54:31 +0900

    Feat: item에 이미지 미리보기 기능 추가

--- app/src/main/java/com/myboard/util/BindingAdapter.kt
+++ app/src/main/java/com/myboard/util/BindingAdapter.kt
@@ -1,10 +1,27 @@
 package com.myboard.util
 
+import android.graphics.BitmapFactory
+import android.util.Base64
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.myboard.R
 import java.util.Date
 
 @BindingAdapter("date")
 fun TextView.setDate(date: Date?){
     text = DateUtil.convertPrintDateString(date)
+}
+
+@BindingAdapter("img")
+fun ImageView.setImage(img : String?){
+    img?.let {
+        if(img!= ""){
+            val encodedByte = Base64.decode(img, Base64.DEFAULT)
+            setImageBitmap(BitmapFactory.decodeByteArray(encodedByte,0,encodedByte.size))
+        }else{
+            setImageResource(R.drawable.baseline_image_search_24)
+        }
+    }
+
 }
